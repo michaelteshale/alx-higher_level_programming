b3f1ca9161c753718d00eec7fdb35b14ef39a854
michaelteshale <teshalemichael@gmail.com>
2022-07-01 00:27:35 -0700

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    argv = sys.argv[1:]
-    argv_count = len(argv)
-    index = 1
-    res = 0
-    while index <= argv_count:
-        res += int(sys.argv[index])
-        index += 1
-    print("{:d}".format(res))
+    import hidden_4
+    for s in dir(hidden_4):
+        if s[:2] != "__":
+             print("{:s}".format(s))
